50732a20ef8e2315cb5364d22bba96c4ff8a4fc8
zachpfaltzgraff <zachpfaltzgraff@gmail.com>
2025-11-24 15:39:04 -0500

    craeted flutter

---
+++ loan_calculator/lib/components/calculator_interface.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class CalculatorInterface extends StatefulWidget {
+  const CalculatorInterface({super.key});
+
+  @override
+  State<CalculatorInterface> createState() => _CalculatorInterfaceState();
+}
+
+class _CalculatorInterfaceState extends State<CalculatorInterface> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
